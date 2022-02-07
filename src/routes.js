31e02375f082b1538686685b314c86026c95e2d3
eugenio-cyber <wesleyeugenio3@gmail.com>
2022-02-07 16:05:03 -0300

    Organizando o caminho do arquivo

--- src/routes.js
+++ src/routes.js
@@ -1,8 +1,8 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 
-import Home from "./pages/Home";
-import Links from "./pages/Links";
-import Error from "./pages/Error";
+import Home from "./pages/Home/index";
+import Links from "./pages/Links/index";
+import Error from "./pages/Error/index";
 
 export default function RoutesApp() {
   return (
